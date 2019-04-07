d9bb1e855ed16d618e232a92677230642467e390
rakeshadloori <rjuidev@gmail.com>
2019-04-07 03:18:41 -0700

    first

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const figlet = require('figlet');
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+    figlet('Docker is amazing! Wow!!!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        res.end(data)
+    });
+}
+
+const server = http.createServer(handler);
+
+server.listen(port, err =>{
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('listening on port 3000');
+    }
+})
